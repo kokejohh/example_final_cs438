192c035c4fba753bf0e879cf66f57cd15ff6c624
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2023-12-04 00:29:29 +0700

    update: database

--- database/phone_shop.sql
+++ database/phone_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 16, 2023 at 10:14 AM
+-- Generation Time: Dec 04, 2023 at 12:28 AM
 -- Server version: 8.0.35-0ubuntu0.22.04.1
 -- PHP Version: 8.1.2-1ubuntu2.14
 
@@ -29,14 +29,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cart` (
   `id` int NOT NULL,
-  `user_id` int NOT NULL,
+  `uid` int NOT NULL,
   `product_id` int NOT NULL,
-  `amount` int NOT NULL,
-  `total` float NOT NULL,
-  `note` text NOT NULL,
-  `create_date` date NOT NULL,
-  `update_date` date NOT NULL
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `uid`, `product_id`, `create_date`) VALUES
+(30, 10007, 20, '2023-12-03 22:50:52'),
+(33, 10005, 18, '2023-12-03 23:04:16'),
+(34, 10005, 13, '2023-12-03 23:04:25'),
+(35, 10005, 18, '2023-12-03 23:04:30'),
+(36, 10005, 18, '2023-12-03 23:05:05'),
+(37, 10009, 13, '2023-12-03 23:25:24'),
+(38, 10009, 22, '2023-12-03 23:25:34');
 
 -- --------------------------------------------------------
 
@@ -50,9 +59,20 @@ CREATE TABLE `product` (
   `price` float NOT NULL,
   `description` text NOT NULL,
   `image` varchar(100) NOT NULL,
-  `create_date` date NOT NULL,
-  `update_date` date NOT NULL
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `price`, `description`, `image`, `create_date`, `update_date`) VALUES
+(13, '12', 12, '21fhfg', '12.png', '2023-12-03 06:19:55', '2023-12-03 23:46:30'),
+(18, '2525', 20, '25256', '2525.png', '2023-12-03 06:24:35', '2023-12-04 00:16:41'),
+(20, 'you', 25, 'ou', 'you.png', '2023-12-03 15:01:47', '2023-12-03 23:41:33'),
+(22, 'zeen23', 100, 'wo', 'zeen.png', '2023-12-03 15:48:59', '2023-12-04 00:16:00'),
+(23, 'zrr', 45, '2342gfd', 'zrr.png', '2023-12-03 22:49:37', '2023-12-03 22:49:37');
 
 -- --------------------------------------------------------
 
@@ -61,14 +81,25 @@ CREATE TABLE `product` (
 --
 
 CREATE TABLE `user` (
-  `id` int NOT NULL,
+  `uid` int NOT NULL,
   `username` varchar(100) NOT NULL,
-  `firstname` varchar(100) NOT NULL,
+  `firstname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `lastname` varchar(100) NOT NULL,
-  `age` int DEFAULT NULL,
-  `gid` int NOT NULL,
   `role` enum('admin','manager','customer') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`uid`, `username`, `firstname`, `lastname`, `role`) VALUES
+(1000, 'admin', 'admin', 'admin', 'admin'),
+(1001, 'john', 'john', 'cena', 'admin'),
+(10005, 'pthiprangsi', 'pitchakorn', 'thiprangsi', 'customer'),
+(10006, 'njobber', 'new', 'jobber', 'manager'),
+(10007, 'mceni', 'mjor', 'ceni', 'customer'),
+(10008, 'jjoh', 'joh', 'joh', 'admin'),
+(10009, 'koke', NULL, 'koke', 'customer');
 
 --
 -- Indexes for dumped tables
@@ -78,7 +109,9 @@ CREATE TABLE `user` (
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_product_id` (`product_id`),
+  ADD KEY `fk_uid` (`uid`);
 
 --
 -- Indexes for table `product`
@@ -90,7 +123,29 @@ ALTER TABLE `product`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `uid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10010;
 
 --
 -- Constraints for dumped tables
@@ -100,8 +155,8 @@ ALTER TABLE `user`
 -- Constraints for table `cart`
 --
 ALTER TABLE `cart`
-  ADD CONSTRAINT `fk_product_id` FOREIGN KEY (`id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
